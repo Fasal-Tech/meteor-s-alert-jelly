6da03ded8b8e4617edd48d36d48277a0bb9eb408
Julian Ćwirko <julian.cwirko@gmail.com>
2015-04-18 16:27:58 +0200

    git url

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     'summary': 'Jelly effect for s-alert - simple and fancy notifications / alerts for Meteor.',
     'version': '1.0.0',
-    'git': 'https://github.com/juliancwirko/meteor-s-alert.git',
+    'git': 'https://github.com/juliancwirko/meteor-s-alert-jelly.git',
     'name': 'juliancwirko:s-alert-jelly'
 });
 
